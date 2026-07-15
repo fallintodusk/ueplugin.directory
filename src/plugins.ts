12851713465ced15458e154cc8ef274b5dd991a9
Alis <fallintodusk@proton.me>
2026-07-15 18:28:16 +0300

    Add ALIS plugin repository

--- src/plugins.ts
+++ src/plugins.ts
@@ -488,5 +488,6 @@ export const PLUGIN_DATA = [
 	"https://github.com/ArmainAP/Unreal-Engine-Widget-Spline-System",
 	"https://github.com/eanticev/niagara-destruction-driver",
 	"https://github.com/CommitAndChill/BlueprintTaskForge",
-	"https://github.com/Ciberusps/UHLDebugSystem"
+	"https://github.com/Ciberusps/UHLDebugSystem",
+	"https://github.com/fallintodusk/alis"
 ]
